0d9698c12ea38a40417f23a899303cc15c6677d2
Augustin Mauroy <augustin.mauroy@outlook.fr>
2024-11-21 22:05:07 +0100

    update biome things

--- build/snapshots.ts
+++ build/snapshots.ts
@@ -1,18 +1,17 @@
 import { basename, dirname, extname, join } from 'node:path';
 import { snapshot } from 'node:test';
 
-
 snapshot.setResolveSnapshotPath(generateSnapshotPath);
 /**
  * @param testFilePath '/tmp/foo.test.js'
  * @returns '/tmp/foo.test.snap.cjs'
  */
 function generateSnapshotPath(testFilePath?: string) {
-  if (!testFilePath) return '';
+	if (!testFilePath) return '';
 
-  const ext = extname(testFilePath);
-  const filename = basename(testFilePath, ext);
-  const base = dirname(testFilePath);
+	const ext = extname(testFilePath);
+	const filename = basename(testFilePath, ext);
+	const base = dirname(testFilePath);
 
-  return join(base, `${filename}.snap.cjs`);
+	return join(base, `${filename}.snap.cjs`);
 }
