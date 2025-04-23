91165c35b84746271e35f360c74d8f7b61ad53d0
Jacob Smith <3012099+JakobJingleheimer@users.noreply.github.com>
2025-04-23 18:59:26 +0200

    fix(correct-ts-specifiers): use `module.registerHooks` (for bundling) (#38)

---
+++ recipes/correct-ts-specifiers/src/nodejs.d.ts
@@ -0,0 +1,18 @@
+// TODO: Delete this file once https://github.com/DefinitelyTyped/DefinitelyTyped/pull/72122
+// is released in @types/node@22.15.x
+
+import type { ResolveHook, LoadHook } from 'node:module';
+
+declare module "module" {
+	namespace Module {
+		function registerHooks(hooks: {
+			resolve?: ResolveHook,
+			load?: LoadHook,
+		}): ModuleHooks;
+
+		class ModuleHooks {
+			constructor(resolve?: ResolveHook, load?: LoadHook);
+			deregister(): void;
+		}
+	}
+}
