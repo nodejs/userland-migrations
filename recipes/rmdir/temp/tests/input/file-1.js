7751acdcfd6da6674c438fbb4b0773daa1deab3b
Augustin Mauroy <97875033+AugustinMauroy@users.noreply.github.com>
2025-07-17 23:11:20 +0200

    WIP

--- recipes/rmdir/temp/tests/input/file-1.js
+++
@@ -1,9 +0,0 @@
-const fs = require("node:fs");
-
-const pathName = "path/to/directory";
-
-fs.rmdir(pathName, { recursive: true }, () => { });
-fs.rmdirSync(pathName, { recursive: true });
-fs.promises.rmdir(pathName, { recursive: true });
-fs.rmdir(pathName, { recursive: false }); // should not be transformed
-fs.rmdir(pathName); // should not be transformed
