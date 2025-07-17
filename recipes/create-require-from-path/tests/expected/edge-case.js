2b5c199dbf25744facc64d8aa5101f043e37d110
Augustin Mauroy <97875033+AugustinMauroy@users.noreply.github.com>
2025-07-17 15:56:46 +0200

    handle `let`, `var`, `const`

---
+++ recipes/create-require-from-path/tests/expected/edge-case.js
@@ -0,0 +1,5 @@
+const { createRequire } = require('module');
+
+var r1 = createRequire('/path/to/module');
+let r2 = createRequire('/path/to/module');
+const r3 = createRequire('/path/to/module');
---
+++ recipes/create-require-from-path/tests/input/edge-case.js
@@ -0,0 +1,5 @@
+const { createRequireFromPath } = require('module');
+
+var r1 = createRequireFromPath('/path/to/module');
+let r2 = createRequireFromPath('/path/to/module');
+const r3 = createRequireFromPath('/path/to/module');
