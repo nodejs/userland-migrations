3addfb3ec09293c595e439a87d12f25a9c47e9f7
Augustin Mauroy <97875033+AugustinMauroy@users.noreply.github.com>
2025-08-03 15:26:00 +0200

    url-parse(`node-url-to-whatwg-url`): setup `url-parse`

---
+++ recipes/node-url-to-whatwg-url/tests/url-parse/expected/file-4.js
@@ -0,0 +1,12 @@
+const { parse: urlParse } = require('node:url');
+
+const myURL = new URL('https://example.com/path?query=string#hash');
+
+const auth = `${myURL.username}:${myURL.password}`;
+const urlAuth = `${myURL.username}:${myURL.password}`;
+
+const path = `${myURL.pathname}${myURL.search}`;
+const urlPath = `${myURL.pathname}${myURL.search}`;
+
+const hostname = myURL.hostname.replace(/^\[|\]$/, '');
+const urlHostname = myURL.hostname.replace(/^\[|\]$/, '');
