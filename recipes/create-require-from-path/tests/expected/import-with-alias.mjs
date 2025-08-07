1043274a734e8f3ba2557ece11464eb4ab0ddef9
Bruno Rodrigues <swe@brunocroh.com>
2025-08-07 15:34:06 +0100

    chore(create-require-from-path): improve test scenario

--- recipes/create-require-from-path/tests/expected/import-with-alias.mjs
+++ recipes/create-require-from-path/tests/expected/import-with-alias.mjs
@@ -1,3 +1,3 @@
-import { createRequire as foo } from "../index.mjs";
+import { other as bar, createRequire as foo } from "../index.mjs";
 
 const r3 = foo("/path/to/module");
--- recipes/create-require-from-path/tests/input/import-with-alias.mjs
+++ recipes/create-require-from-path/tests/input/import-with-alias.mjs
@@ -1,3 +1,3 @@
-import { createRequireFromPath as foo } from "../index.mjs";
+import { other as bar, createRequireFromPath as foo } from "../index.mjs";
 
 const r3 = foo("/path/to/module");
