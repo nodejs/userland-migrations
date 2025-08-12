50193e029925874eb2f05bc0e8665330283b969a
Augustin Mauroy <97875033+AugustinMauroy@users.noreply.github.com>
2025-08-12 20:49:51 +0200

    add more supported case for `url-format`

--- recipes/node-url-to-whatwg-url/tests/url-format/expected/file-1.js
+++ recipes/node-url-to-whatwg-url/tests/url-format/expected/file-1.js
@@ -1,3 +1,9 @@
 const url = require('node:url');
 
 const str = new URL('https://example.com/some/path?page=1').toString();
+
+const foo = 'https';
+
+const search = '?page=1';
+
+const str2 = new URL(`${foo}://example.com/some/path${search}`).toString();
