a59787eaecbcab2989c31f4b9ed740084eb77a32
Bruno Rodrigues <swe@brunocroh.com>
2025-08-20 13:34:08 +0100

    feat(util/remove-lines): remove duplicated instructions before process

--- utils/src/ast-grep/remove-lines.ts
+++ utils/src/ast-grep/remove-lines.ts
@@ -3,6 +3,17 @@ import type { Range } from "@codemod.com/jssg-types/main";
 export function removeLines(sourceCode: string, ranges: Range[]) {
 	const lines = sourceCode.split("\n");
 
+	const uniqueRemoves = new Map<string, Range>();
+
+	for (const range of ranges) {
+		const key = `start-line:${range.start.line},end-line:${range.end.line},start-column:${range.start.column},end-column:${range.end.column}`;
+		if (!uniqueRemoves.has(key)) {
+			uniqueRemoves.set(key, range);
+		}
+	}
+
+	ranges = Array.from(uniqueRemoves.values());
+
 	let removeCounter = 0;
 	for (const range of ranges) {
 		const start = range.start.line - removeCounter;
