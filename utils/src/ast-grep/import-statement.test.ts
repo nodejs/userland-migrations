e9df0577133f09a6980e1292b4d9c24a5553a145
Augustin Mauroy <97875033+AugustinMauroy@users.noreply.github.com>
2025-08-21 11:16:03 +0200

    update

--- utils/src/ast-grep/import-statement.test.ts
+++ utils/src/ast-grep/import-statement.test.ts
@@ -4,7 +4,8 @@ import astGrep from '@ast-grep/napi';
 import dedent from 'dedent';
 import {
 	getNodeImportStatements,
-	getDefaultImportIdentifier
+	getDefaultImportIdentifier,
+	getNodeImportCalls
 } from "./import-statement.ts";
 
 describe("import-statement", () => {
@@ -39,6 +40,56 @@ describe("import-statement", () => {
 		const utilImports = getNodeImportStatements(ast, 'util');
 		assert.strictEqual(utilImports.length, 1);
 		assert.strictEqual(utilImports[0].field('source')?.text(), '"node:util"');
+	});
+
+	it("should return import calls", () => {
+		const code = dedent`
+			const fs = await import('fs');
+			var { join } = await import('node:path');
+			let { spawn } = await import("child_process");
+			const { styleText } = await import("node:util");
+			await import("no:assignment");
+			await import(variable);
+			await import(\`backticks\`);
+			import("no:assignment");
+			import(variable);
+			import(\`backticks\`);
+		`;
+		const ast = astGrep.parse(astGrep.Lang.JavaScript, code);
+
+		const fsCalls = getNodeImportCalls(ast, 'fs');
+		assert.strictEqual(fsCalls.length, 1);
+		const fsCallExpr = fsCalls[0].field('value')?.children()[1]; // await_expression -> call_expression
+		assert.strictEqual(fsCallExpr?.field('function')?.text(), 'import');
+		assert.strictEqual(fsCallExpr?.field('arguments')?.find({ rule: { kind: "string" } })?.text(), "'fs'");
+
+		const pathCalls = getNodeImportCalls(ast, 'path');
+		assert.strictEqual(pathCalls.length, 1);
+		const pathCallExpr = pathCalls[0].field('value')?.children()[1]; // await_expression -> call_expression
+		assert.strictEqual(pathCallExpr?.field('function')?.text(), 'import');
+		assert.strictEqual(pathCallExpr?.field('arguments')?.find({ rule: { kind: "string" } })?.text(), "'node:path'");
+
+		const childProcessCalls = getNodeImportCalls(ast, 'child_process');
+		assert.strictEqual(childProcessCalls.length, 1);
+		const childProcessCallExpr = childProcessCalls[0].field('value')?.children()[1]; // await_expression -> call_expression
+		assert.strictEqual(childProcessCallExpr?.field('function')?.text(), 'import');
+		assert.strictEqual(childProcessCallExpr?.field('arguments')?.find({ rule: { kind: "string" } })?.text(), '"child_process"');
+
+		const utilCalls = getNodeImportCalls(ast, 'util');
+		assert.strictEqual(utilCalls.length, 1);
+		const utilCallExpr = utilCalls[0].field('value')?.children()[1]; // await_expression -> call_expression
+		assert.strictEqual(utilCallExpr?.field('function')?.text(), 'import');
+		assert.strictEqual(utilCallExpr?.field('arguments')?.find({ rule: { kind: "string" } })?.text(), '"node:util"');
+	});
+
+	it("shouldn't catch pending promises during import calls", () => {
+		const code = dedent`
+			const pending = import("node:module");
+		`;
+		const ast = astGrep.parse(astGrep.Lang.JavaScript, code);
+
+		const moduleCalls = getNodeImportCalls(ast, 'module');
+		assert.strictEqual(moduleCalls.length, 0, "Pending import calls should not be caught");
 	});
 
     it("should handle getDefaultImportIdentifier", () => {
--- utils/src/ast-grep/import-statement.ts
+++ utils/src/ast-grep/import-statement.ts
@@ -14,6 +14,60 @@ export const getNodeImportStatements = (rootNode: SgRoot, nodeModuleName: string
 			}
 		});
 
+/**
+ * We just catch `variable_declarator` nodes that use `import` to import a module
+ * Because a simple `import('nodeAPI')` don't do anything, so in codemod context we don't need to
+ * catch those.
+ *
+ * We also don't catch pending promises, like `const pending = import("node:module");`
+ * because it's will became to complex to handle in codemod context. (storing var name, checking is method is used, etc.)
+ */
+export const getNodeImportCalls = (rootNode: SgRoot, nodeModuleName: string): SgNode[] =>
+	rootNode
+	.root()
+	.findAll({
+		rule: {
+			kind: "variable_declarator",
+			all: [
+				{
+					has: {
+						field: "name",
+						any: [
+							{ kind: "object_pattern" },
+							{ kind: "identifier" }
+						]
+					}
+				},
+				{
+					has: {
+						field: "value",
+						kind: "await_expression",
+						has: {
+							kind: "call_expression",
+							all: [
+								{
+									has: {
+										field: "function",
+										kind: "import"
+									}
+								},
+								{
+									has: {
+										field: "arguments",
+										kind: "arguments",
+										has: {
+											kind: "string",
+											regex: `^['"](node:)?${nodeModuleName}['"]$`
+										}
+									}
+								}
+							]
+						}
+					}
+				}
+			]
+		}
+	});
 
 /**
  * Get the default import identifier from an import statement
