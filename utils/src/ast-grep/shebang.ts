52d32771b27a41efb933a41a0269a5a75b92d158
Augustin Mauroy <97875033+AugustinMauroy@users.noreply.github.com>
2025-08-30 10:46:01 +0200

    feat(`utils`): clean shebang

--- utils/src/ast-grep/shebang.ts
+++ utils/src/ast-grep/shebang.ts
@@ -31,32 +31,34 @@ export const getShebang = (root: SgRoot) =>
  */
 export const replaceNodeJsArgs = (root: SgRoot, argsToValues: Record<string, string>, edits: Edit[]) => {
 	const shebang = getShebang(root);
+
 	if (!shebang) return;
 
 	const text = shebang.text();
+	// Find the "node" argument in the shebang
 	const nodeMatch = text.match(/\bnode(\.exe)?\b/);
 
 	if (!nodeMatch) return;
 
+	// We only touch to something after node because before it's env thing
 	const nodeIdx = nodeMatch.index! + nodeMatch[0].length;
 	const beforeNode = text.slice(0, nodeIdx);
 	let afterNode = text.slice(nodeIdx);
 
-	const sortedArgs = Object.keys(argsToValues);
-
-	for (const argC of sortedArgs) {
+	for (const argC of Object.keys(argsToValues)) {
 		// Escape special regex characters in arg
 		const esc = argC.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
 		const regex = new RegExp(`(\\s+)(["']?)${esc}(["']?)(?=\\s|$)`, 'g');
-		let replaced = false;
+
+		// handling quote and whitespaces
 		const newAfterNode = afterNode.replace(regex, (_unused, ws, q1, q2) => {
-			replaced = true;
 			const replacement = argsToValues[argC];
+
 			return `${ws}${q1}${replacement}${q2}`;
 		});
-		if (replaced && newAfterNode !== afterNode) {
-			const newText = beforeNode + newAfterNode;
-			edits.push(shebang.replace(newText));
+
+		if (newAfterNode !== afterNode) {
+			edits.push(shebang.replace(beforeNode + newAfterNode));
 			afterNode = newAfterNode;
 		}
 	}
