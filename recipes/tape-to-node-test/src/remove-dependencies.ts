c0037e1c8347f7f4d06ab2d2ba3e8bfba365f3ae
Augustin Mauroy <97875033+AugustinMauroy@users.noreply.github.com>
2025-12-15 20:28:13 +0100

    remove dep

---
+++ recipes/tape-to-node-test/src/remove-dependencies.ts
@@ -0,0 +1,8 @@
+import removeDependencies from '@nodejs/codemod-utils/remove-dependencies';
+
+/**
+ * Remove tape and @types/tape dependencies from package.json
+ */
+export default function removeTapeDependencies(): string | null {
+	return removeDependencies(['tape', '@types/tape']);
+}
