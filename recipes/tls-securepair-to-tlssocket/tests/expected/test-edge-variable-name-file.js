700224b42fe27bb4d964d62869d1ce3d8d305039
menace31 <maxime.devillet01@gmail.com>
2025-12-16 14:11:56 +0100

    fix tests

---
+++ recipes/tls-securepair-to-tlssocket/tests/expected/test-edge-variable-name-file.js
@@ -0,0 +1,7 @@
+const { TLSSocket } = require('node:tls');
+
+// Completely arbitrary variable name
+const socket = new TLSSocket(socket);
+
+// Usage
+socket.doSomething();
---
+++ recipes/tls-securepair-to-tlssocket/tests/expected/test-multiple-variable-file.js
@@ -0,0 +1,7 @@
+const tls = require('node:tls');
+
+const socket = new tls.TLSSocket(socket);
+const mySocket = new tls.TLSSocket(socket);
+const secureSocketInstance = new tls.TLSSocket(socket);
+
+// Specific cleanup for each variable
