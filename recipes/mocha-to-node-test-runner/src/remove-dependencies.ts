5a357233a3ac1086e742b6289d7355b82331f42a
Xavier Stouder <xavier@stouder.io>
2026-03-11 20:54:16 +0100

    update remove dependencies step

--- recipes/mocha-to-node-test-runner/src/remove-dependencies.ts
+++ recipes/mocha-to-node-test-runner/src/remove-dependencies.ts
@@ -1,8 +1,13 @@
+import type { Transform } from '@codemod.com/jssg-types/main';
+import type Json from '@codemod.com/jssg-types/langs/json';
 import removeDependencies from '@nodejs/codemod-utils/remove-dependencies';
 
-/**
- * Remove chalk and @types/chalk dependencies from package.json
- */
-export default function removeMochaDependencies(): string | null {
-	return removeDependencies(['mocha', '@types/mocha']);
-}
+const transform: Transform<Json> = async (root) => {
+	return removeDependencies(['mocha', '@types/mocha'], {
+		packageJsonPath: root.filename(),
+		runInstall: false,
+		persistFileWrite: false,
+	});
+};
+
+export default transform;
