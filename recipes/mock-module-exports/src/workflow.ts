327691c0a4b303d9ff4367c07763d3f183e6c81c
Bruno Rodrigues <swe@brunocroh.com>
2026-03-14 13:54:49 +0000

    feat: add support to replace variables

--- recipes/mock-module-exports/src/workflow.ts
+++ recipes/mock-module-exports/src/workflow.ts
@@ -1,4 +1,4 @@
-import type { SgRoot, Edit, SgNode } from '@codemod.com/jssg-types/main';
+import type { SgRoot, Edit, SgNode, Kinds } from '@codemod.com/jssg-types/main';
 import type JS from '@codemod.com/jssg-types/langs/javascript';
 import { getModuleDependencies } from '@nodejs/codemod-utils/ast-grep/module-dependencies';
 import { resolveBindingPath } from '@nodejs/codemod-utils/ast-grep/resolve-binding-path';
@@ -7,6 +7,136 @@ import {
 	getLineIndent,
 } from '@nodejs/codemod-utils/ast-grep/indent';
 import { EOL } from 'node:os';
+
+type QueueEvent = {
+	event: keyof typeof parsers;
+	handler: () => Edit[];
+};
+
+const queue: QueueEvent[] = [];
+
+type Pair = {
+	before: SgNode<JS, 'pair'>;
+	after: string;
+};
+
+type ExportedValue = {
+	node: SgNode<JS, Kinds<JS>>;
+	default: Pair;
+	named: Pair[] | undefined;
+};
+const exportedValues: Map<number, ExportedValue> = new Map();
+
+type Parsers = {
+	parseOptions: (optionsNode: SgNode<JS, Kinds<JS>>) => undefined;
+	defaultExport: (node: SgNode<JS, Kinds<JS>>) => Edit[];
+	resolveVariables: (node: SgNode<JS, Kinds<JS>>) => undefined;
+	namedExports: (optionsNode: SgNode<JS, Kinds<JS>>) => Edit[];
+};
+
+const parsers: Parsers = {
+	parseOptions: (optionsNode: SgNode<JS, Kinds<JS>>): undefined => {
+		switch (optionsNode.kind()) {
+			case 'object':
+				queue.unshift({
+					event: 'defaultExport',
+					handler: () => parsers.defaultExport(optionsNode),
+				});
+				queue.unshift({
+					event: 'namedExports',
+					handler: () => parsers.namedExports(optionsNode),
+				});
+			case 'identifier':
+				queue.unshift({
+					event: 'resolveVariables',
+					handler: () => parsers.resolveVariables(optionsNode),
+				});
+		}
+	},
+	resolveVariables: (node: SgNode<JS, Kinds<JS>>) => {
+		const definition = node.definition();
+		if (!definition) return;
+
+		switch (definition.node.parent().kind()) {
+			case 'variable_declarator':
+				const parent = definition.node.parent<'variable_declarator'>();
+				queue.unshift({
+					event: 'parseOptions',
+					handler: () => parsers.parseOptions(parent.field('value')),
+				});
+				break;
+			default:
+				throw new Error('unhandled scenario');
+		}
+	},
+	defaultExport: (node: SgNode<JS, Kinds<JS>>): Edit[] => {
+		const edits: Edit[] = [];
+		const defaultExport = node.find<'pair'>({
+			rule: {
+				kind: 'pair',
+				has: {
+					field: 'key',
+					kind: 'property_identifier',
+					regex: 'defaultExport',
+				},
+			},
+		});
+		if (defaultExport) {
+			const change = {
+				before: defaultExport,
+				after: `default: ${defaultExport?.field('value').text()}`,
+			};
+
+			if (!exportedValues.has(node.id())) {
+				exportedValues.set(node.id(), {
+					node,
+					default: change,
+					named: [],
+				});
+				return;
+			}
+
+			const n = exportedValues.get(node.id());
+			n.default = change;
+		}
+		return edits;
+	},
+	namedExports: (node: SgNode<JS, Kinds<JS>>): Edit[] => {
+		const edits: Edit[] = [];
+		const namedExport = node.find<'pair'>({
+			rule: {
+				kind: 'pair',
+				has: {
+					field: 'key',
+					kind: 'property_identifier',
+					regex: 'namedExport',
+				},
+			},
+		});
+
+		if (namedExport) {
+			if (!exportedValues.has(node.id())) {
+				exportedValues.set(node.id(), {
+					node,
+					default: undefined,
+					named: [],
+				});
+			}
+
+			const pairs = exportedValues.get(node.id()).named;
+
+			for (const namedPair of namedExport.field('value').children()) {
+				if (!namedPair.is('pair')) continue;
+
+				pairs.push({
+					before: namedPair,
+					after: namedPair.text(),
+				});
+			}
+		}
+		return edits;
+	},
+};
 
 export default function transform(root: SgRoot<JS>): string | null {
 	const rootNode = root.root();
@@ -41,60 +171,41 @@ export default function transform(root: SgRoot<JS>): string | null {
 
 		if (args.length < 2) continue;
 		const optionsArg = args[1];
-		const indentUnit = detectIndentUnit(rootNode.text());
-		const pairs = [];
+		queue.unshift({
+			event: 'parseOptions',
+			handler: () => parsers.parseOptions(optionsArg),
+		});
+	}
 
-		if (optionsArg.is('object')) {
-			const defaultExport = optionsArg.find<'pair'>({
-				rule: {
-					kind: 'pair',
-					has: {
-						field: 'key',
-						kind: 'property_identifier',
-						regex: 'defaultExport',
-					},
-				},
-			});
+	const indentUnit = detectIndentUnit(rootNode.text());
+	while (queue.length) {
+		const event = queue.at(-1);
+		event.handler();
+		queue.pop();
+	}
 
-			if (defaultExport) {
-				pairs.push(`default: ${defaultExport?.field('value').text()}`);
-			}
+	for (const [_nodeId, change] of Array.from(exportedValues)) {
+		const indentLevel = getLineIndent(
+			rootNode.text(),
+			change.node.range().start.index,
+		);
 
-			const namedExport = optionsArg.find<'pair'>({
-				rule: {
-					kind: 'pair',
-					has: {
-						field: 'key',
-						kind: 'property_identifier',
-						regex: 'namedExport',
-					},
-				},
-			});
+		const exportsLevel = `${indentLevel}${indentUnit}`;
+		const innerExports = `${exportsLevel}${indentUnit}`;
 
-			if (namedExport) {
-				for (const namedPair of namedExport.field('value').children()) {
-					if (!namedPair.is('pair')) continue;
-					pairs.push(namedPair.text());
-				}
-			}
+		let newValue = `{${EOL}` + `${exportsLevel}exports: {${EOL}`;
 
-			const indentLevel = getLineIndent(
-				rootNode.text(),
-				optionsArg.range().start.index,
-			);
-
-			const exportsLevel = `${indentLevel}${indentUnit}`;
-			const innerExports = `${exportsLevel}${indentUnit}`;
-
-			const newValue =
-				`{${EOL}`
-				+ `${exportsLevel}exports: {${EOL}`
-				+ `${innerExports}${pairs.join(`,${EOL}${innerExports}`)}` + `,${EOL}`
-				+ `${exportsLevel}},${EOL}`
-				+ `${indentLevel}}`;
-
-			edits.push(optionsArg.replace(newValue));
+		if (change.default?.after) {
+			newValue += `${innerExports}${change.default.after},${EOL}`;
 		}
+
+		if (change.named?.length) {
+			newValue += `${innerExports}${change.named.map((t) => t.after).join(`,${EOL}${innerExports}`)},${EOL}`;
+		}
+
+		newValue += `${exportsLevel}},${EOL}` + `${indentLevel}}`;
+
+		edits.push(change.node.replace(newValue));
 	}
 
 	let sourceCode = rootNode.commitEdits(edits);
