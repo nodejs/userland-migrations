1a6d90848e3925edc40c4d236c1d3157d9a7fc03
Augustin Mauroy <97875033+AugustinMauroy@users.noreply.github.com>
2026-03-20 22:35:03 +0100

    feat(`cjs-to-esm`): scaffold codemod  (#340)

---
+++ recipes/cjs-to-esm/src/export-process.ts
@@ -0,0 +1,15 @@
+import type { SgRoot, Edit } from '@codemod.com/jssg-types/main';
+import type JS from '@codemod.com/jssg-types/langs/javascript';
+
+/**
+ * @see https://github.com/nodejs/package-examples/tree/main/guide/05-cjs-esm-migration/migrating-exports
+ */
+export default function transform(root: SgRoot<JS>): string | null {
+	const rootNode = root.root();
+	const edits: Edit[] = [];
+
+	// do some stuff
+
+	if (!edits.length) return null;
+	return rootNode.commitEdits(edits);
+}
