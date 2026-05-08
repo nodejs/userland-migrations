a0ee08bf53c943c1984c1024b5bcaaaddd2d7937
Augustin Mauroy <97875033+AugustinMauroy@users.noreply.github.com>
2026-05-08 11:28:48 +0200

    update test

--- recipes/correct-ts-specifiers/tests/e2e/expected.ts
+++ recipes/correct-ts-specifiers/tests/e2e/expected.ts
@@ -1,13 +1,29 @@
+import { URL } from 'node:url';
+
+import { bar } from '@dep/bar';
+import { foo } from 'foo';
+
 import { Bird } from './Bird/index.ts';
 import { Cat } from './Cat.ts';
+import { Dog } from '…/Dog/index.mjs';
+import { baseUrl } from '#config.js';
+import { qux } from './qux.js/index.ts';
 
 export type { Zed } from './zed.d.ts';
+
+// should.js be unchanged
 
 const nil = await import('./nil.ts');
 
 const bird = new Bird('Tweety');
 const cat = new Cat('Milo');
+const dog = new Dog('Otis');
+
+export const makeLink = (path: URL) => (new URL(path, baseUrl)).href;
 
 console.log('bird:', bird);
 console.log('cat:', cat);
+console.log('dog:', dog);
+console.log('foo:', foo);
+console.log('bar:', bar);
 console.log('nil:', nil);
